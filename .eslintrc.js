cf46a6c454cf7b113677bcec56094264498d6b4f
Max Wong <maxw@MBP-353-maxw.local>
2024-02-22 10:48:05 -0800

    fix eslint ident

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,22 @@ module.exports = {
   root: true,
   extends: 'airbnb-base',
   env: {
-    browser: true,
+      browser: true,
   },
   parser: '@babel/eslint-parser',
   parserOptions: {
-    allowImportExportEverywhere: true,
-    sourceType: 'module',
-    requireConfigFile: false,
+      allowImportExportEverywhere: true,
+      sourceType: 'module',
+      requireConfigFile: false,
   },
   rules: {
-    'indent': ['error', 4, { "SwitchCase": 1 }],
-    'max-len': ['error', { code: 120 }],
-    'no-param-reassign': [2, { props: false }],
-    'linebreak-style': ['error', 'unix'],
-    'import/extensions': ['error', {
-      js: 'always',
-    }],
-    'no-unused-vars': ['warn', { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+      'indent': ['error', 4, { "SwitchCase": 1 }],
+      'max-len': ['error', { code: 120 }],
+      'no-param-reassign': [2, { props: false }],
+      'linebreak-style': ['error', 'unix'],
+      'import/extensions': ['error', {
+          js: 'always',
+      }],
+      'no-unused-vars': ['warn', { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
   },
 };
