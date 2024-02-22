18ac47d2c8cd55fd95a339e8de58ac4a6956a7f3
Max Wong <maxw@MBP-353-maxw.local>
2024-02-22 10:52:22 -0800

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,13 +11,13 @@ module.exports = {
         requireConfigFile: false,
     },
     rules: {
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'indent': ['error', 4, { SwitchCase: 1 }],
         'max-len': ['error', { code: 120 }],
         'no-param-reassign': [2, { props: false }],
         'linebreak-style': ['error', 'unix'],
         'import/extensions': ['error', {
-          js: 'always',
-      }],
-      'no-unused-vars': ['warn', { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+            js: 'always',
+        }],
+        'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', 'ignoreRestSiblings': false }],
   },
 };
