d04c16396b622b36ed538b8b5935481ff23cbf00
Max Wong <maxw@MBP-353-maxw.local>
2024-02-22 10:54:15 -0800

    revert eslintrc to og

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-    root: true,
-    extends: 'airbnb-base',
-    env: {
-        browser: true,
-    },
-    parser: '@babel/eslint-parser',
-    parserOptions: {
-        allowImportExportEverywhere: true,
-        sourceType: 'module',
-        requireConfigFile: false,
-    },
-    rules: {
-        'indent': ['error', 4, { SwitchCase: 1 }],
-        'max-len': ['error', { code: 120 }],
-        'no-param-reassign': [2, { props: false }],
-        'linebreak-style': ['error', 'unix'],
-        'import/extensions': ['error', {
-            js: 'always',
-        }],
-        'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', 'ignoreRestSiblings': false }],
+  root: true,
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  rules: {
+    // allow reassigning param
+    'no-param-reassign': [2, { props: false }],
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': ['error', {
+      js: 'always',
+    }],
   },
 };
